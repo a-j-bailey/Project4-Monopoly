3a39d3efb769ccb0151ff34d5002df7a6b4d63e0
stephanthegreat <stephannash103@gmail.com>
2017-11-10 18:54:03 -0500

    I don't even know

--- src/application/Utility.java
+++ src/application/Utility.java
@@ -2,6 +2,18 @@ package application;
 
 public class Utility extends Property{
 	
+	
+	
+	/**
+	 * 
+	 * @param position
+	 * @param color
+	 * @param name
+	 * @param price
+	 * @param mortgage
+	 * @param rent
+	 * Constructor for utility properties (including railroads)
+	 */
 	public Utility(int position, String color, String name, int price, int mortgage, int rent) {
 		super(position, color, name, price, mortgage, rent);
 		// TODO Auto-generated constructor stub
