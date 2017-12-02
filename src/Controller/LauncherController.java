4c2c232f8989148ad9304053ecb3bdbcb4f83a09
Adam Bailey <33302008+adamjbailey@users.noreply.github.com>
2017-12-02 14:35:02 -0500

    Completed Launcher
    It now checks to make sure a name has been entered before running game
    so that it doesnt crash on an empty string.

--- src/Controller/LauncherController.java
+++ src/Controller/LauncherController.java
@@ -5,19 +5,61 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import application.Game;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LauncherController implements Initializable{
-
+	
+	@FXML
+	private VBox launcherVbox;
+	
+	private RadioButton[] buttons = new RadioButton[4];
+	private TextField[] textFields = new TextField[4];
+	private HBox[] addPlayers = new HBox[4];
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		
+		for(int i=0; i<4; i++){
+			TextField text = new TextField();
+			text.setPromptText("Name");
+			text.setDisable(true);
+			
+			RadioButton button = new RadioButton(" Player " + (i+3));
+			button.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			    @Override
+			    public void changed(ObservableValue<? extends Boolean> obs, Boolean wasPreviouslySelected, Boolean isNowSelected) {
+			        text.setDisable(!text.isDisable());
+			    }
+			});
+			
+			buttons[i] = button;
+			textFields[i] = text;
+			
+			HBox box = new HBox();
+			box.getChildren().add(button);
+			box.getChildren().add(text);
+			box.setAlignment(Pos.CENTER);
+			box.setSpacing(20);
+			addPlayers[i] = box;
+		}
+		
+		launcherVbox.setPadding(new Insets(10, 0, 0, -32));
+		launcherVbox.setSpacing(10);
+		launcherVbox.getChildren().addAll(addPlayers);
+		
 		System.out.println(" -- Launcher Initialized -- ");
 	}
 		
@@ -32,64 +74,41 @@ public class LauncherController implements Initializable{
 	private TextField p2Name;
 	
 	@FXML
-	private RadioButton p3;
-	@FXML
-	private TextField p3Name;
-	public void changeP3(ActionEvent event){
-		p3Name.setDisable(!p3Name.isDisabled());
-	}
-	
-	@FXML
-	private RadioButton p4;
-	@FXML
-	private TextField p4Name;
-	public void changeP4(ActionEvent event){
-		p4Name.setDisable(!p4Name.isDisabled());
-	}
-	
-	@FXML
-	private RadioButton p5;
-	@FXML
-	private TextField p5Name;
-	public void changeP5(ActionEvent event){
-		p5Name.setDisable(!p5Name.isDisabled());
-	}
-	
-	@FXML
-	private RadioButton p6;
-	@FXML
-	private TextField p6Name;
-	public void changeP6(ActionEvent event){
-		p6Name.setDisable(!p6Name.isDisabled());
-	}
-	
-	@FXML
 	private Button play;
 	public void launchGame(ActionEvent event){
 		
-		//TODO: what if user doesn't enter names or leaves places empty?
-
 		ArrayList<String> playerNames = new ArrayList<String>();
-		playerNames.add(p1Name.getText());
-		playerNames.add(p2Name.getText());
-		if(!p3Name.isDisabled()){
-			playerNames.add(p3Name.getText());
+
+		if(!p1Name.getText().equals("")){
+			playerNames.add(p1Name.getText());
+			p1Name.setStyle("-fx-control-inner-background: #FFFFFF");
+		} else {
+			p1Name.setStyle("-fx-control-inner-background: #FF8A80");
 		}
-		if(!p4Name.isDisabled()){
-			playerNames.add(p4Name.getText());
+		if(!p2Name.getText().equals("")){
+			playerNames.add(p2Name.getText());
+			p2Name.setStyle("-fx-control-inner-background: #FFFFFF");
+		} else {
+			p2Name.setStyle("-fx-control-inner-background: #FF8A80");
 		}
-		if(!p5Name.isDisabled()){
-			playerNames.add(p5Name.getText());
-		}
-		if(!p6Name.isDisabled()){
-			playerNames.add(p6Name.getText());
+		if(playerNames.size() == 2){
+			for(int i=0; i<4; i++){
+				if(buttons[i].isSelected()){
+					if(!textFields[i].getText().equals("")){
+						playerNames.add(textFields[i].getText());
+					}
+				}
+			}
+			
+			Stage stage = (Stage) play.getScene().getWindow();
+			stage.close();
+			
+			System.out.println(" -- Initializing Game -- ");
+			
+			new Game(playerNames);
 		}
 		
-		Stage stage = (Stage) play.getScene().getWindow();
-		stage.close();
 		
-		System.out.println(" -- Initializing Game -- ");
 		
-		new Game(playerNames);
 	}
 }
